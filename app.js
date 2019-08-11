47be7e4d1f6dcbfb80752f330b0189a0b5e52c9b
Moses Peter <mohemos@live.com>
2019-08-12 00:19:59 +0100

    Update

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express'),
     app = express();
 
+app.get('/',(req,res)=>{
+    res.send("<h1>Hello mum</h1>");
+});
 
 app.listen(1000,(err)=>{
     err && console.log("An error occured",err);
